d7e635528063d698a6d43d8574f6b0f801173f1f
Minh Bui <tranduyminh.bui@gmail.com>
2019-10-06 17:21:43 +1030

    side menu

--- js/main.js
+++ js/main.js
@@ -1,66 +1,10 @@
-// const slides = document.querySelectorAll('.slide');
-// const next = document.querySelector('#next');
-// const prev = document.querySelector('#prev');
-// const auto = false;
-// const intervalTime = 5000;
-// let slideInterval;
-
-// const nextSlide = () => {
-//     const current = document.querySelector('.current');
-//     current.classList.remove('current');
-//     // check for next slide
-//     if(current.nextElementSibling) {
-//         // add current to next sibling
-//         current.nextElementSibling.classList.add('current');
-//     }
-//     else {
-//         // Add current to start
-//         slides[0].classList.add('current');
-//     }
-//     // setTimeout(() => current.classList.remove('current'), 200);
-// }
-
-// const prevSlide = () => {
-//     const current = document.querySelector('.current');
-//     current.classList.remove('current');
-//     // check for prev slide
-//     if(current.previousElementSibling) {
-//         // add current to prev sibling
-//         current.previousElementSibling.classList.add('current');
-//     }
-//     else {
-//         // Add current to last
-//         slides[slides.length -1].classList.add('current');
-//     }
-//     // setTimeout(() => current.classList.remove('current'), 200);
-// }
-
-// // Button events
-// next.addEventListener('click', e => {
-//     nextSlide();
-//     if(auto) {
-//         clearInterval(slideInterval);
-//         slideInterval = setInterval(nextSlide, intervalTime);
-//     }
-// });
-// prev.addEventListener('click', e => {
-//     prevSlide();
-//     if(auto) {
-//         clearInterval(slideInterval);
-//         slideInterval = setInterval(nextSlide, intervalTime);
-//     }
-// });
-
-// // auto slide
-// if(auto) {
-//     // run next slide at interval time
-//     slideInterval = setInterval(nextSlide, intervalTime);
-// }
-
 let gallery = document.getElementById("photos");
 let modal = document.getElementById("modal");
 let modalClose = document.getElementById("modal-close");
 let modalImg = document.getElementById("modal-img");
+
+let menu = document.getElementById("burger");
+let burgerMenu = document.getElementById("burgerMenu");
 
 for (let i = 0; i < 27; i++) {
   let e = document.createElement("img");
@@ -77,4 +21,9 @@ for (let i = 0; i < 27; i++) {
 
 modalClose.onclick = function() {
   modal.setAttribute("style", "display: none; visibility: hidden;");
+};
+
+menu.onclick = () => {
+  burgerMenu.classList.toggle("visible");
+  menu.classList.toggle("close");
 };
