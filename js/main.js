91d4ed2b5b6b708b3732503bf5702a4c88303304
Minh Bui <tranduyminh.bui@gmail.com>
2019-11-03 23:59:57 +1030

    stories modal

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let modalImg = document.getElementById("modal-img");
 
 let menu = document.getElementById("burger");
 let burgerMenu = document.getElementById("burgerMenu");
+let sidebarTitle = document.getElementById("sidebar_title");
 
 for (let i = 0; i < 27; i++) {
   let e = document.createElement("img");
@@ -19,6 +20,7 @@ for (let i = 0; i < 27; i++) {
   gallery.appendChild(e);
 }
 
+// close modal X button
 modalClose.onclick = function() {
   modal.setAttribute("style", "display: none; visibility: hidden;");
 };
@@ -26,4 +28,35 @@ modalClose.onclick = function() {
 menu.onclick = () => {
   burgerMenu.classList.toggle("visible");
   menu.classList.toggle("close");
+};
+
+// Story section modal
+let card1 = document.getElementById("card-1");
+let modalSt = document.getElementById("modal-story");
+let modalStClose = document.getElementById("modal-story-close");
+
+let modalStTitle = document.getElementById("modal-story-title");
+let modalStText = document.getElementById("modal-story-text");
+let modalStImg = document.getElementById("modal-story-img");
+
+card1.onclick = function() {
+  // modalStImg.src = "gallery/thumb/ex.jpg";
+  modalSt.setAttribute("style", "display: block; visibility: visible;");
+  modalStTitle.innerText = stories[0].title;
+  modalStText.innerText = stories[0].text;
+  for (let i = 0; i < 7; i++) {
+    let pict = document.createElement("img");
+    pict.src = `stories-img/1/${i}.jpg`;
+
+    modalStImg.appendChild(pict);
+  }
+};
+
+modalStClose.onclick = function() {
+  modalSt.setAttribute("style", "display: none; visibility: hidden;");
+  modalStTitle.innerText = "";
+  modalStText.innerText = "";
+  while (modalStImg.firstChild) {
+    modalStImg.firstChild.remove();
+  }
 };
